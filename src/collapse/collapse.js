789486e980e9feb33c0772c8c264a92b61dded05
Jason <jason.the.adams@gmail.com>
2013-08-28 21:43:04 -0400

    Bootstrap 3.0 Collapse Update
    Do not apply this, as it does not work yet, but I'm working to update to use the 'collapse', 'collapsing', and 'collapse in' CSS classes. Honestly, I'm guessing as to their order of use, especially the '.collapse .in' pair.
    
    I removed the FixUpHeight function as it no longer seemed efficient to have a generic function, due to the changing CSS classes.
    
    The major issue I'm having is that the promise resolves for expand and collapse are happening at the wrong time (e.g., when it collapses the expand .then function fires; nothing happens as isCollapsed is in the wrong state). Maybe I'm doing something dumb though. :P

--- src/collapse/collapse.js
+++ src/collapse/collapse.js
@@ -7,15 +7,9 @@ angular.module('ui.bootstrap.collapse',['ui.bootstrap.transition'])
   // Unfortunately if you do this while the CSS transitions are specified (i.e. in the CSS class
   // "collapse") then you trigger a change to height 0 in between.
   // The fix is to remove the "collapse" CSS class while changing the height back to auto - phew!
-  var fixUpHeight = function(scope, element, height) {
-    // We remove the collapse CSS class to prevent a transition when we change to height: auto
-    element.removeClass('collapse');
-    element.css({ height: height });
-    // It appears that  reading offsetWidth makes the browser realise that we have changed the
-    // height already :-/
-    var x = element[0].offsetWidth;
-    element.addClass('collapse');
-  };
+
+  // NOTE:
+  // Removed the fixUpHeight function, for now, as more class manipulation was needed than before.
 
   return {
     link: function(scope, element, attrs) {
@@ -31,9 +25,15 @@ angular.module('ui.bootstrap.collapse',['ui.bootstrap.transition'])
         if (element[0].scrollHeight !== 0) {
           if (!isCollapsed) {
             if (initialAnimSkip) {
-              fixUpHeight(scope, element, element[0].scrollHeight + 'px');
+              element.css({height: element[0].scrollHeight + 'px'});
+              element.removeClass('collapse in collapsing');
+              var x = element[0].offsetWidth;
+              element.addClass('collapse in');
             } else {
-              fixUpHeight(scope, element, 'auto');
+              element.css({height: 'auto'});
+              element.removeClass('collapse in collapsing');
+              var x = element[0].offsetWidth;
+              element.addClass('collapse in');
             }
           }
         }
@@ -55,8 +55,8 @@ angular.module('ui.bootstrap.collapse',['ui.bootstrap.transition'])
         }
         currentTransition = $transition(element,change);
         currentTransition.then(
-          function() { currentTransition = undefined; },
-          function() { currentTransition = undefined; }
+          function() { currentTransition = undefined;},
+          function() { currentTransition = undefined;}
         );
         return currentTransition;
       };
@@ -65,7 +65,10 @@ angular.module('ui.bootstrap.collapse',['ui.bootstrap.transition'])
         if (initialAnimSkip) {
           initialAnimSkip = false;
           if ( !isCollapsed ) {
-            fixUpHeight(scope, element, 'auto');
+            element.removeClass('collapsing');
+            var x = element[0].offsetWidth;
+            element.addClass('collapse in');
+            element.css({height: 'auto'});
           }
         } else {
           doTransition({ height : element[0].scrollHeight + 'px' })
@@ -73,8 +76,14 @@ angular.module('ui.bootstrap.collapse',['ui.bootstrap.transition'])
             // This check ensures that we don't accidentally update the height if the user has closed
             // the group while the animation was still running
             if ( !isCollapsed ) {
-              fixUpHeight(scope, element, 'auto');
+              // Switch to post-transition display: block
+              element.removeClass('collapsing in');
+              var x = element[0].offsetWidth;
+              element.addClass('collapse in');
+              element.css({height: 'auto'});
             }
+            // Promise testing purposes only
+            // console.log("Fired expand promise");
           });
         }
         isCollapsed = false;
@@ -84,10 +93,25 @@ angular.module('ui.bootstrap.collapse',['ui.bootstrap.transition'])
         isCollapsed = true;
         if (initialAnimSkip) {
           initialAnimSkip = false;
-          fixUpHeight(scope, element, 0);
+          element.removeClass('collapsing in');
+          var x = element[0].offsetWidth;
+          element.addClass('collapse');
+
         } else {
-          fixUpHeight(scope, element, element[0].scrollHeight + 'px');
-          doTransition({'height':'0'});
+          // Switch to collapsing for the transition
+          element.removeClass('collapse in');
+          var x = element[0].offsetWidth;
+          element.addClass('collapsing');
+          
+          doTransition({'height':'0px'})
+              .then(function() {
+                // After the transition, remove collapsing and add collapse
+                if (isCollapsed) {
+                    element.removeClass('collapsing');
+                    element.addClass('collapse');
+                }
+                console.log("Fired collapse promise");
+              });
         }
       };
     }
